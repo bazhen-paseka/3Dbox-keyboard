409e48b9f3ea135872a0abce57afd431ce560a66
Бажен Левковець <bazhen.paseka@gmail.com>
2021-03-11 11:15:43 +0200

    folder Local_config added

---
+++ Drivers/Local_config/3Dbox-keyboards-local-config.h
@@ -0,0 +1,13 @@
+//******************************************************************************************
+#ifndef 3DBOX_KEYBOARD_LOCAL_CONFIG_H_INCLUDED
+#define 3DBOX_KEYBOARD_LOCAL_CONFIG_H_INCLUDED
+//******************************************************************************************
+
+	#define SOFT_VERSION	300
+
+	#define VERSION_DAY		11
+	#define VERSION_MONTH	3
+	#define VERSION_YEAR	2021
+
+//******************************************************************************************
+#endif	//	3DBOX_KEYBOARD_LOCAL_CONFIG_H_INCLUDED
---
+++ Drivers/Local_config/pcf8574_local_config.h
@@ -0,0 +1,19 @@
+/*
+ * pcf8574_local_config.h
+ *
+ *  Created on: 18 ???. 2020 ?.
+ *      Author: Paseka
+ */
+
+#ifndef LOCAL_CONFIG_PCF8574_LOCAL_CONFIG_H_
+#define LOCAL_CONFIG_PCF8574_LOCAL_CONFIG_H_
+
+//*****************************************************
+
+	#define I2C_ADR_PCF_0 0x20
+	#define I2C_ADR_PCF_1 0x38
+
+//*****************************************************
+
+
+#endif /* LOCAL_CONFIG_PCF8574_LOCAL_CONFIG_H_ */
